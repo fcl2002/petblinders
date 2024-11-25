c302399ac6e0a428981a3a91430da9db50705bbd
Fernando <fernandolasmar94@gmail.com>
2024-11-25 14:04:12 -0300

    first commit + spring initializr

---
+++ petblinders/src/main/java/com/ninja/startup/marketplace/petblinders/PetblindersApplication.java
@@ -0,0 +1,13 @@
+package com.ninja.startup.marketplace.petblinders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetblindersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetblindersApplication.class, args);
+	}
+
+}
---
+++ petblinders/src/test/java/com/ninja/startup/marketplace/petblinders/PetblindersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninja.startup.marketplace.petblinders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetblindersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
